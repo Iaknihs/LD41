07c73ae2630497311549ceaf53657aa83bbcc3ae
schruff <schruff@dbis.rwth-aachen.de>
2018-04-21 10:35:16 +0200

    Implemented simple Platformer Base

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+	public float jumpForce;
+
+    public CharacterController controller;
+	public Vector3 moveDirection;
+	public float gravityScale;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float ytemp = moveDirection.y;
+		moveDirection = (transform.forward * Input.GetAxis ("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+		moveDirection = moveDirection.normalized * moveSpeed;
+		moveDirection.y = ytemp;
+
+		if(controller.isGrounded){
+			moveDirection.y = 0f;
+			if (Input.GetButtonDown("Jump")) {
+				moveDirection.y = jumpForce;
+			}
+		}
+
+		moveDirection.y = moveDirection.y + (Physics.gravity.y*gravityScale*Time.deltaTime);
+
+
+		controller.Move(moveDirection*Time.deltaTime);
+	}
+}
